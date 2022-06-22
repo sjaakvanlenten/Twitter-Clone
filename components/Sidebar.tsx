c9a32aa15e9be57f2f50bf5053249e2a50a78c24
unknown <sjaakvanlenten@hotmail.com>
2022-06-22 03:27:42 +0200

    first commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from "@heroicons/react/outline";
+import SidebarRow from "./SidebarRow";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+function Sidebar() {
+  const { data: session } = useSession();
+
+  return (
+    <div className="col-span-2 flex flex-col items-center px-4 md:items-start">
+      <img
+        className="h-10 w-10 m-3"
+        src="https://links.papareact.com/drq"
+        alt=""
+      />
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={HashtagIcon} title="Explore" />
+      <SidebarRow Icon={BellIcon} title="Notifications" />
+      <SidebarRow Icon={MailIcon} title="Messages" />
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SidebarRow Icon={CollectionIcon} title="Lists" />
+      <SidebarRow
+        Icon={UserIcon}
+        title={session ? "Sign out" : "Sign in"}
+        onClick={session ? signOut : signIn}
+      />
+
+      <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+    </div>
+  );
+}
+
+export default Sidebar;
